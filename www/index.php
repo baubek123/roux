4dbfc95907b51a3729f8037699b0e432aa907e08
Baubek Toktagazin <bogus_88@mail.ru>
2014-09-23 16:21:23 +0600

    Changed the title

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>Roux Conference</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -22,7 +22,7 @@
 
 <body>
 
-
+    <p>Welcome to the Roux Conference</p>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
