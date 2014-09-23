abc84c7f5416c4726f6e2c668e47995ce7e6c6eb
Baubek Toktagazin <bogus_88@mail.ru>
2014-09-23 17:06:30 +0600

    Basic scaffold for website complete

---
+++ www/schedule.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>    
+    <title>Roux Academy Conference 2013 -- Schedule</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,400italic,700,700italic,300,300italic' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+
+<body id="schedule">
+    <section class="container">
+        <div class="content row">
+            <section class="main col col-lg-8">
+                
+            </section> <!-- main -->
+            <section class="sidebar col col-lg-4">
+
+            </section> <!-- sidebar -->
+        </div> <!-- content -->
+    </section> <!--container-->
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
