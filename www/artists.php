1fa69572c1dcfc21e97f30c8f3370521f6a646ba
Baubek Toktagazin <bogus_88@mail.ru>
2014-09-25 11:00:15 +0600

    Artists page ready

--- www/artists.php
+++ www/artists.php
@@ -11,17 +11,19 @@
     <link href="css/custom.css" rel="stylesheet">
 </head>
 
-<body id="artists">
+<body id="artists" data-spy="scroll" data-target=".scrollspy">
     <section class="container">
         <div class="content row">
             <?php include "php/header.php"; ?>
             <section class="main col col-lg-8">
-                
+            <?php include "php/article-artistlist.php"; ?>
             </section> <!-- main -->
             <section class="sidebar col col-lg-4">
-
+            <?php include "php/aside-register.php"; ?>
+            <?php include "php/aside-lastyear.php"; ?>
+            <?php include "php/aside-accordion.php"; ?>
             </section> <!-- sidebar -->
-                <?php include "php/footer.php"; ?>            
+            <?php include "php/footer.php"; ?>
         </div> <!-- content -->
     </section> <!--container-->
     
